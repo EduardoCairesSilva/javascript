48bad695e8dde549576d45990c4010a1cdf44187
Eduardo Caires Silva <yandudu743@gmail.com>
2024-05-13 23:53:31 -0300

    Exemplos switch/case

---
+++ js/switch-case.js
@@ -0,0 +1,37 @@
+function getDiaSemanaTexto (diaSemana) {
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = '';
+            return diaSemanaTexto = '';
+    }
+}
+
+
+const data = new Date('2022-05-26 00:00:00');
+let diaSemana = data.getDay();
+let diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+
+console.log(diaSemanaTexto);
