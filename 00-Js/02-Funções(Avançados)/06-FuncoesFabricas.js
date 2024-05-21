a5514990fcb28d3e988a836e92f995aa0c0dd239
Eduardo Caires <yandudu743@gmail.com>
2024-05-21 05:44:58 -0300

    Adicionando funções construtoras.

---
+++ 00-Js/02-Funções(Avançados)/06-FuncoesFabricas.js
@@ -0,0 +1,32 @@
+function criaPessoa(nome, sobrenome, altura, peso) {
+    return {
+        nome,
+        sobrenome,
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`;
+        },
+        
+        // Setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.shift();
+            this.sobrenome = valor.join(' ');
+            console.log(valor);
+        },
+
+        fala(assunto) {
+            return `${this.nome} está ${assunto}!`;
+        },
+        altura: altura,
+        peso: peso,
+        // Getter
+        get imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
+        }
+    };
+}
+
+const p1 = criaPessoa('Eduardo', 'Caires', 1.8, 80);
+p1.nomeCompleto = 'Alice Caires Silva'
+console.log(p1.nomeCompleto)
