66813da10a8b9fc3f75ebc28a75c1cf4d6077e04
Eduardo Caires <yandudu743@gmail.com>
2024-05-21 00:39:05 -0300

    Adicionando retorno de função

---
+++ 00-Js/02-Funções(Avançados)/03-RetornFunction.js
@@ -0,0 +1,18 @@
+// return
+// Retorna um valor
+// Termina a função
+
+function criaMultiplicador(multiplicador) {
+    // Multplicador
+    return function(n) {
+        return n * multiplicador;
+    };
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(3));
+console.log(quadriplica(4));
