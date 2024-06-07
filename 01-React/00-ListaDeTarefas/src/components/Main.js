55c7db13e22dad61cd1412b4b8b1f52371589dfc
Eduardo Caires <yandudu743@gmail.com>
2024-06-07 02:55:24 -0300

    Adicionando todas as tarefas no localStorage

--- 01-React/00-ListaDeTarefas/src/components/Main.js
+++ 01-React/00-ListaDeTarefas/src/components/Main.js
@@ -66,6 +66,22 @@ export default class Main extends Component {
     });
   };
 
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+    if (!tarefas) return;
+
+    this.setState({ tarefas })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tarefas } = this.state;
+
+    if (tarefas === prevState.tarefas) return;
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }
+
   render() {
     const { novaTarefa, tarefas } = this.state;
 
