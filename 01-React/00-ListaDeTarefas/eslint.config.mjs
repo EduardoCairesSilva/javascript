5c27592d552a0eddf49de9a57a7c4b9aa80bf17a
Eduardo Caires <yandudu743@gmail.com>
2024-06-06 22:30:07 -0300

    Criando funcionalidade de deletar e editar as tarefas.

---
+++ 01-React/00-ListaDeTarefas/eslint.config.mjs
@@ -0,0 +1,47 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+];
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
---
+++ 01-React/00-ListaDeTarefas/src/components/Main.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+
+// Form
+import { FaPlus } from 'react-icons/fa';
+
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
+import './Main.css'
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+    tarefas: [],
+    index: -1,
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      });
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  handleEdit = (e, index) => {
+    const { tarefas } = this.state;
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index]
+    });
+  };
+
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
+    const novasTarefas = [...tarefas];
+    novasTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novasTarefas]
+    });
+  };
+
+  render() {
+    const { novaTarefa, tarefas } = this.state;
+
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+
+        <form onSubmit={this.handleSubmit} action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
+        </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa, index) => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit
+                  onClick={(e) => this.handleEdit(e, index)}
+                  className="edit"
+                />
+
+                <FaWindowClose
+                  onClick={(e) => this.handleDelete(e, index)}
+                  className="delete"
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
