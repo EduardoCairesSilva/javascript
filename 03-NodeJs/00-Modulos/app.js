0c437c6e6ac43aba57e5f7782e84ef2ea13d4a39
Eduardo Caires <yandudu743@gmail.com>
2024-06-08 00:12:35 -0300

    Iniciando em NodeJS e adicionando módulos pt1.

---
+++ 03-NodeJs/00-Modulos/app.js
@@ -0,0 +1,6 @@
+// const { nome, sobrenome, falaNome } = require('./mod1');
+// falaNome();
+
+const { Pessoa } = require('./mod1');
+const p1 = new Pessoa('Eduardo');
+console.log(p1);
---
+++ 03-NodeJs/00-Modulos/mod1.js
@@ -0,0 +1,19 @@
+// const nome = 'Eduardo';
+// const sobrenome = 'Caires';
+
+// const falaNome = () => {
+//     console.log(nome, sobrenome);
+// };
+
+// exports.nome = nome;
+// exports.sobrenome = sobrenome;
+// exports.falaNome = falaNome;
+// this.qualquerCoisa = 'Qualquer coisa!'
+
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome;
+    }
+};
+
+exports.Pessoa = Pessoa;
